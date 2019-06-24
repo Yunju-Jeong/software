0398a1c499d4ca40d8fcca062d99ce66a29a1719
Yunju-Jeong <52024446+Yunju-Jeong@users.noreply.github.com>
2019-06-24 22:06:11 +0900

    Add files via upload

--- ML_usingPublicData.py
+++ ML_usingPublicData.py
@@ -4,20 +4,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('고속도로교통사고현황.csv', encoding='CP949')
-list1 = df.values.tolist()
-ar = np.array(list1)
-xData = ar[:, 0]-2000
-yData = ar[:, 1]
+ar = df.to_numpy()
+xData = ar[:, 0]-2000 # 연도에서 2000을 뺀다. (예) 2010 ==> 10
+yData = ar[:, 1]      # 사고 수
 
-W = tf.Variable(tf.random_uniform([1], -100, 100))
-b = tf.Variable(tf.random_uniform([1], -100, 100))
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
-H = W * X + b
-cost = tf.reduce_mean(tf.square(H-Y))
-a = tf.Variable(0.01)
-optimizer = tf.train.GradientDescentOptimizer(a)
-train = optimizer.minimize(cost)
+W = tf.Variable(tf.random_uniform([1], -100, 100)) # W 변수 선언하고 랜덤 넘버로 초기화
+b = tf.Variable(tf.random_uniform([1], -100, 100)) # b 변수 선언하고 랜덤 넘버로 초기화
+X = tf.placeholder(tf.float32)                     # X 변수 선언
+Y = tf.placeholder(tf.float32)                     # Y 변수 선언
+H = W * X + b                                      # 선형 회귀 방정식 선언
+cost = tf.reduce_mean(tf.square(H-Y))              # 비용함수식 선언
+a = tf.Variable(0.01)                              # Learning rate 선언 및 초기화
+optimizer = tf.train.GradientDescentOptimizer(a)   # 경사하강 방법을 최적화 도구로 이용
+train = optimizer.minimize(cost)                   # 경사하강으로 비용이 최소화
+
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
